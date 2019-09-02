c35291c80deef9bb12034c2b26a523c7a5db53da
JM Marcastel <don@marcastel.com>
2019-09-02 07:06:27 +0200

    General udpate

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Little Snitch rulesets
 
+Feel free to use the blacklist below... the whitelist is probably too specific :smile:
+
 | List      | Subscription URL       |
 | --------- | ---------------------- |
 | Blacklist | https://bit.ly/2MQukhj |
--- build.sh
+++ build.sh
@@ -2,21 +2,20 @@
 
 typeset jq='jq .'; whence -q jq || jq=cat
 
-function mkwhitelist {
+function build_blacklist {
+
+    (   print_header 'ISLE Blacklist' 'Disabled Internet resources for any process (and any user).'
+        print_disabled
+        print_footer
+    )   | $jq > blacklist.lsrules
+}
+
+function build_whitelist {
 
     (   print_header 'ISLE Whitelist' 'Allowed HTTP(S) targets for any process (and any user).'
         print_allowed
         print_footer
     )   | $jq > whitelist.lsrules
-}
-
-function print_footer {
-    printf ']\n}\n'
-}
-
-function print_header {
-    typeset name="$1" brief="$2"
-    printf '{\n"name": "%s",\n"description": "%s",\n"rules": [\n' "$name" "$brief"
 }
 
 function print_allowed {
@@ -34,6 +33,11 @@ function print_allowed {
         count+=$(grep -v '^#' $file | grep -v '^$' | wc -l)
 
     done
+
+    # Make sure we have something to process
+    (( count > 0 )) || return
+
+    printf '"rules": [\n'
 
     # Process all possible definition files
     for file in ${all[@]}; do
@@ -57,8 +61,66 @@ function print_allowed {
 
     done
 
+    printf ']\n'
+
 }
 
-mkwhitelist
+function print_disabled {
+
+    typeset fmt='{ "action" : "allow", "ports" : [ "80", "443" ], "process" : "any", "protocol" : "tcp", "remote-%s" : "%s" }'
+    typeset all=( addresses domains hosts )
+
+    # Process all possible definition files
+    for file in ${all[@]}; do
+
+        # Make sure the definition file exists
+        typeset path=blacklist.$file; [[ -f $path ]] || continue
+
+        # Count number of definition lines in file
+        count=$(grep -v '^#' $path | grep -v '^$' | wc -l)
+
+        # Make sure we have something to process
+        (( count > 0 )) || return
+
+        printf '"denied-remote-%s": [' $file
+
+        # Read the definition file, line by line
+        typeset line; cat $path | while read line; do
+
+            # Ignore empty lines and comments
+            [[ -z $line || ${line:0:1} == '#' ]] && continue
+
+            # Determine if we should append a JSON comma
+            typeset comma=','; (( -- count > 0 )) || comma=
+
+            # Output the JSON line
+            printf '"%s"%s\n' "$line" "$comma"
+
+        done
+
+        printf '],\n'
+
+    done
+
+    # Print dummy field so we don't have to clear the last comma above
+    printf '"owner": "me"\n'
+
+}
+
+function print_footer {
+
+    printf '}\n'
+
+}
+
+function print_header {
+
+    typeset name="$1" brief="$2"
+    printf '{\n"name": "%s",\n"description": "%s",\n' "$name" "$brief"
+
+}
+
+#build_whitelist
+build_blacklist
 
 # vim: nospell spelllang=en
